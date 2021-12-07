df6abc2f77dbb6eeddd1f1abd126435a250498d7
Brandon Hughey <brandon@creativedotdesign.com>
2021-12-07 11:34:52 -0500

    docs: text update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
   <div class="w-11/12 mx-auto md:w-10/12 lg:w-3/4 xl:w-2/3 xl:text-lg 2xl:w-7/12">
     Plan your next character build in the Elder Scrolls V Skyrim. Choose how you will
     spend your perk points by navigating to each of the main skill trees in the
-    menu above.
+    menu above. More features coming soon.
   </div>
 
   <div class="w-11/12 mx-auto mt-8 md:w-10/12 md:mt-10 lg:w-3/4 xl:w-2/3 2xl:w-7/12">
