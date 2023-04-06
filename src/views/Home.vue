5f6c0c77885c80ee604df033ba30c135cf6a74f2
Brandon Hughey <brandon@creativedotdesign.com>
2023-04-06 12:51:31 -0400

    style: re-work home page description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,19 @@ import Total from '@/components/Total.vue';
     Elder Scrolls V Skyrim Character Builder
   </h1>
 
-  <div class="w-11/12 mx-auto md:w-10/12 lg:w-3/4 xl:w-2/3 xl:text-lg 2xl:w-7/12">
-    Plan your next character build in the Elder Scrolls V Skyrim. Choose how you will
-    spend your perk points by navigating to each of the main skill trees in the
-    menu above. More features coming soon.
+  <div class="w-11/12 mx-auto flex flex-col md:w-10/12 lg:w-3/4 xl:w-2/3 xl:text-lg 2xl:w-7/12">
+    <p>
+      Plan your next character build in the Elder Scrolls V Skyrim. Choose how you will
+      spend your perk points by navigating to each of the main skill trees in the
+      menu above.
+    </p>
+
+    <p class="mt-8">
+      Once you have chosen a skill tree, all perks under that skill will become visible.
+      Simply click the perk you want to add to your build. To remove it, simply click the X
+      icon that becomes visible next to the title, or hit the Clear All Perks button to remove
+      them all from your build.
+    </p>
   </div>
 
   <div class="w-11/12 mx-auto mt-8 md:w-10/12 md:mt-10 lg:w-3/4 xl:w-2/3 2xl:w-7/12">
