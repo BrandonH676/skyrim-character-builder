edd73f9359d25caba3db330a2ba7fa6f35029a67
Brandon Hughey <brandon@creativedotdesign.com>
2023-04-17 11:34:58 -0400

    fix: tailwind class name conflict fix (again)

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,6 @@ module.exports = {
         '0.625': '0.625rem',
       },
       height: {
-        '0-percent': '0%',
         '140': '35rem',
         '112': '28rem',
         'min-content': 'min-content',
